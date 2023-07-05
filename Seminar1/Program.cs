fc3f4e9cb16ce619d0fc8fd0bda8d7132abc56eb
FedorSherbinin <fredersherb@gmail.com>
2023-07-05 19:42:34 +0300

    add Task 3

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -32,3 +32,18 @@
 
 // Console.Write("max = ");
 // Console.WriteLine(max);
+
+// Task 3. Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Imput a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num % 2 == 0)
+// {
+//     Console.WriteLine("YES");
+// }
+// else
+// {
+//     Console.WriteLine("NO");
+// }
