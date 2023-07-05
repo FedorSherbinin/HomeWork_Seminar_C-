2ccd33413ac2f8e96127b3835617867adac0a74c
FedorSherbinin <fredersherb@gmail.com>
2023-07-05 19:16:12 +0300

    add Task 1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Task 1. Напишите программу, которая на вход принимает два числа и выдаёт большее число.
 
-Console.Write("Imput a first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Imput a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2)
-{
-    Console.WriteLine(num1);
-}
-else
-{
-    Console.WriteLine(num2);
-}
+// if(num1 > num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     Console.WriteLine(num2);
+// }
