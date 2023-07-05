bcad19291741878b7a158b6b35c71c6c8ef0041b
FedorSherbinin <fredersherb@gmail.com>
2023-07-05 20:02:27 +0300

    add Task 4

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -47,3 +47,15 @@
 // {
 //     Console.WriteLine("NO");
 // }
+
+// Task 4. Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Imput a number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = 1; i <= number; i++)
+// {
+//     if(i % 2 == 0)
+//     Console.Write(i + " ");
+// }
